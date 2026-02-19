c905ac90d84ca7b0ded7d4fb5c6e3b3d245dda10
Cosmopolitan IT <mahfuzshah81@gmail.com>
2026-02-19 17:12:17 +0600

    Add files via upload

---
+++ dess/js/main.js
@@ -0,0 +1,392 @@
+﻿/*
+  [JS Index]
+*/
+
+
+/*
+  1. preloader
+  2. to top animation
+  3. page scroll
+  4. navigation
+  5. animate elements
+  6. facts counter 
+  7. google maps zoom ON/OFF
+  8. owl carousel
+  9. swiper slider
+  10. magnificPopup
+  11. items selector
+  12. datepicker
+  13. Select2
+  14. toggle blog panels
+  15. contact modal
+  16. contact form
+  17. reservation form
+  18. blog comment form
+*/
+
+
+$(function() {
+    "use strict";
+	
+	
+    $(window).on("load", function() {
+        // 1. preloader
+        $("#preloader").fadeOut(600);
+        $(".preloader-bg").delay(400).fadeOut(600);
+    });
+	
+    // 2. to top animation
+    $(".to-top-arrow").on("click", function() {
+        $("body, html").animate({
+            scrollTop: 0
+        }, 1000);
+        return false
+    })
+	
+    // 3. page scroll													   
+    $('a[href*="#"]').not('[href="#"]').not('[href="#collapseOne"]').not('[href="#collapseTwo"]').not('[href="#collapseThree"]').not('[href="#collapseFour"]').not('[href="#collapseFive"]').not(
+        '[href="#collapseSix"]').on("click", function() {
+        console.log("click");
+        if (location.pathname.replace(/^\//, "") === this.pathname.replace(/^\//, "") && location.hostname === this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=" + this.hash.slice(1) + "]');
+            if (target.length) {
+                if ($(window).width() < 768) {
+                    $("html, body").animate({
+                        scrollTop: target.offset().top - 80
+                    }, 1000);
+                } else {
+                    $("html, body").animate({
+                        scrollTop: target.offset().top - 80
+                    }, 1000);
+                }
+                return false;
+            }
+        }
+    });
+	
+    // 4. navigation
+    $(".navbar-collapse ul li a.nav-close").on("click", function() {
+        $(".navbar-toggle:visible").click();
+    });
+    // navigation dropdown
+    $(".dropdown-toggle").on("click", function(e) {
+        if ($(document).width() > 768) {
+            e.preventDefault();
+            var url = $(this).attr('href');
+            if (url !== '#') {
+                window.location.href = url;
+            }
+        }
+    });
+	// navigation dropdown scroll fix
+    $(".navbar-toggle").on("click", function () {
+      if ($(window).width() < 992) {
+        $("body").toggleClass("nav-open");
+      }
+    });
+	
+    $(window).on("scroll", function() {
+        // 5. animate elements
+        if ($(this).scrollTop() > 50) {
+            $(".navbar-bg-switch").addClass("navbar-bg-switch-color");
+            $(".main-navigation-bg").addClass("main-navigation-bg-position-primary");
+            $(".navbar-collapse").addClass("navbar-collapse-position-primary");
+            $(".main-navigation").addClass("main-navigation-home-call");
+            $(".navbar-bg-switch").addClass("main-navigation-bg");
+            $(".to-top-arrow").addClass("show");
+			$(".blog-side-launcher").addClass("blog-side-launcher-color");
+        } else {
+            $(".navbar-bg-switch").removeClass("navbar-bg-switch-color");
+            $(".main-navigation-bg").removeClass("main-navigation-bg-position-primary");
+            $(".navbar-collapse").removeClass("navbar-collapse-position-primary");
+            $(".main-navigation").removeClass("main-navigation-home-call");
+            $(".navbar-bg-switch").removeClass("main-navigation-bg");
+            $(".to-top-arrow").removeClass("show");
+			$(".blog-side-launcher").removeClass("blog-side-launcher-color");
+        }
+    });
+	
+    // 6. facts counter
+    $(".facts-counter-number").appear(function() {
+        var count = $(this);
+        count.countTo({
+            from: 0,
+            to: count.html(),
+            speed: 1200,
+            refreshInterval: 60
+        });
+    });
+	
+    // 7. google maps zoom ON/OFF
+    $(".google-maps").on("click", function() {
+        $('.google-maps iframe').css("pointer-events", "auto");
+    });
+    $(".google-maps").on("mouseleave", function() {
+        $('.google-maps iframe').css("pointer-events", "none");
+    });
+	
+    // 8. owl carousel
+    $(".owl-carousel-all").owlCarousel({
+        loop: false,
+        center: false,
+        autoplay: false,
+        autoplaySpeed: 1000,
+        autoplayTimeout: 5000,
+        smartSpeed: 450,
+        nav: false,
+        nav: true,
+        navText: ["<i class='fa ion-chevron-left'></i>", "<i class='fa ion-chevron-right'></i>"],
+        responsive: {
+            0: {
+                items: 1,
+                margin: 20
+            },
+            768: {
+                items: 1,
+                margin: 20
+            },
+            980: {
+                items: 2,
+                margin: 40
+            },
+            1240: {
+                items: 3,
+                margin: 40
+            }
+        }
+    });
+	$(".owl-carousel-restaurant").owlCarousel({
+        loop: false,
+        center: false,
+        autoplay: false,
+        autoplaySpeed: 1000,
+        autoplayTimeout: 5000,
+        smartSpeed: 450,
+        nav: false,
+        nav: true,
+        navText: ["<i class='fa ion-chevron-left'></i>", "<i class='fa ion-chevron-right'></i>"],
+		mouseDrag: false,
+        touchDrag: false,
+        pullDrag: false,
+        responsive: {
+            0: {
+                items: 1,
+                margin: 20
+            },
+            768: {
+                items: 2,
+                margin: 20
+            },
+            980: {
+                items: 3,
+                margin: 40
+            },
+            1240: {
+                items: 3,
+                margin: 40
+            }
+        }
+    });
+	$(".owl-carousel-testimonials").owlCarousel({
+        loop: true,
+        center: true,
+        items: 1,
+        margin: 0,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        autoplayTimeout: 4000,
+        smartSpeed: 450,
+        nav: false,
+        animateOut: "fadeOut",
+        animateIn: "fadeIn"
+    });
+	
+    // 9. swiper slider
+    var swiper1 = new Swiper(".swiper-container-wrapper .swiper-container.swiper1", {
+        preloadImages: false,
+		autoplay: false,
+        init: true,
+        loop: true,
+        speed: 1200,
+        grabCursor: true,
+        mousewheel: false,
+        keyboard: true,
+        simulateTouch: true,
+        parallax: true,
+        effect: "slide",
+        pagination: false,
+        navigation: {
+            nextEl: ".slide-next",
+            prevEl: ".slide-prev"
+        },
+        scrollbar: false
+    });
+	var swiper2 = new Swiper(".swiper-container-wrapper .swiper-container.swiper2", {
+        preloadImages: false,
+        autoplay: false,
+        init: true,
+        loop: false,
+        grabCursor: false,
+        mousewheel: false,
+        keyboard: false,
+        simulateTouch: false,
+        parallax: false,
+        pagination: false,
+        navigation: false
+    });
+	
+    // 10. magnificPopup
+    $(".popup-photo").magnificPopup({
+        type: "image",
+        gallery: {
+            enabled: false,
+            tPrev: "",
+            tNext: "",
+            tCounter: "%curr% / %total%"
+        },
+        removalDelay: 100,
+        mainClass: "mfp-fade",
+        fixedContentPos: false
+    });
+    $(".popup-photo-gallery").each(function() {
+        $(this).magnificPopup({
+            delegate: ".popup-photo-gallery-open",
+            type: "image",
+            gallery: {
+                enabled: true
+            },
+            removalDelay: 100,
+            mainClass: "mfp-fade",
+            fixedContentPos: false
+        });
+    });
+	
+    // 11. items selector
+    const buttons = document.querySelectorAll(".item-button");
+    const menus = document.querySelectorAll(".menu");
+    function handleClick(e) {
+        e.preventDefault();
+        const targetMenu = document.querySelector(`#${this.dataset.target}`);
+        menus.forEach(menu => {
+            menu.classList.remove("menu-visible");
+            targetMenu.classList.add("menu-visible");
+        });
+    }
+    buttons.forEach(button => button.addEventListener("click", handleClick));
+	
+    // 12. datepicker
+    $(".datepicker").datepicker({
+        orientation: "auto"
+    });
+	
+    // 13. Select2
+    $(".select2").select2({
+        minimumResultsForSearch: Infinity
+    });
+	
+    // 14. toggle blog panels
+	$(".blog-side-launcher").on("click", function() {
+        var divClass = $(this).attr("data-id");
+        if ($(this).hasClass("open")) {
+            $(this).removeClass("open");
+            $("." + divClass).addClass("open");
+        } else {
+            $(this).addClass("open");
+            $("." + divClass).addClass("open");
+        }
+    });
+    $(".blog-side-launch, .blog-side-text").on("click", function() {
+        $(".panel-from-left-blog, .panel-overlay-from-right-blog").removeClass("open");
+    });
+	
+	// 15. contact modal
+    $(".contact-modal-launcher, .contact-modal-closer").on("click", function(e) {
+        e.preventDefault();
+        if ($(".contact-modal").hasClass("open")) {
+            $(".contact-modal").removeClass("open");
+        } else {
+            $(".contact-modal").addClass("open");
+        }
+    });
+	
+    // 16. contact form
+    $("form#form").on("submit", function() {
+        $("form#form .error").remove();
+        var s = !1;
+        if ($(".requiredField").each(function() {
+                if ("" === jQuery.trim($(this).val())) $(this).prev("label").text(), $(this).parent().append('<span class="error">This field is required</span>'), $(this).addClass(
+                    "inputError"), s = !0;
+                else if ($(this).hasClass("email")) {
+                    var r = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+                    r.test(jQuery.trim($(this).val())) || ($(this).prev("label").text(), $(this).parent().append('<span class="error">Invalid email address</span>'), $(this).addClass(
+                        "inputError"), s = !0);
+                }
+            }), !s) {
+            $("form#form input.submit").fadeOut("normal", function() {
+                $(this).parent().append("");
+            });
+            var r = $(this).serialize();
+            $.post($(this).attr("action"), r, function() {
+                $("form#form").slideUp("fast", function() {
+                    $(this).before('<div class="success">Your email was sent successfully.</div>');
+                });
+            });
+        }
+        return !1;
+    });
+	
+    // 17. reservation form
+    $("form#form-2").on("submit", function() {
+        $("form#form-2 .error-r").remove();
+        var s = !1;
+        if ($(".requiredField-r").each(function() {
+                if ("" === jQuery.trim($(this).val())) $(this).prev("label").text(), $(this).parent().append('<span class="error-r">This field is required</span>'), $(this).addClass(
+                    "inputError"), s = !0;
+                else if ($(this).hasClass("email")) {
+                    var r = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+                    r.test(jQuery.trim($(this).val())) || ($(this).prev("label").text(), $(this).parent().append('<span class="error-r">Invalid email address</span>'), $(this).addClass(
+                        "inputError"), s = !0);
+                }
+            }), !s) {
+            $("form#form-2 input.submit").fadeOut("normal", function() {
+                $(this).parent().append("");
+            });
+            var r = $(this).serialize();
+            $.post($(this).attr("action"), r, function() {
+                $("form#form-2").slideUp("fast", function() {
+                    $(this).before('<div class="success-r">Your reservation has been received.</div>');
+                });
+            });
+        }
+        return !1;
+    });
+	
+    // 18. blog comment form
+	$("form#form-3").on("submit", function() {
+        $("form#form-3 .error-c").remove();
+        var s = !1;
+        if ($(".requiredField-c").each(function() {
+                if ("" === jQuery.trim($(this).val())) $(this).prev("label").text(), $(this).parent().append('<span class="error-c">This field is required</span>'), $(this).addClass(
+                    "inputError"), s = !0;
+                else if ($(this).hasClass("email")) {
+                    var r = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+                    r.test(jQuery.trim($(this).val())) || ($(this).prev("label").text(), $(this).parent().append('<span class="error-c">Invalid email address</span>'), $(this).addClass(
+                        "inputError"), s = !0);
+                }
+            }), !s) {
+            $("form#form-3 input.submit").fadeOut("normal", function() {
+                $(this).parent().append("");
+            });
+            var r = $(this).serialize();
+            $.post($(this).attr("action"), r, function() {
+                $("form#form-3").slideUp("fast", function() {
+                    $(this).before('<div class="success">Your email was sent successfully.</div>');
+                });
+            });
+        }
+        return !1;
+    });
+	
+	
+});
